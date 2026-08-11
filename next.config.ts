26ae3fceba5f16c59012cbc63f7b50435b0983b7
Rupam Kairi <rupamkairi@hotmail.com>
2026-08-11 12:31:16 +0530

    Add substackcdn.com to remotePatterns in next.config.ts for image optimization.

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "www-cdn.plant-for-the-planet.org",
       },
+      {
+        protocol: "https",
+        hostname: "substackcdn.com",
+      },
     ],
   },
 };
